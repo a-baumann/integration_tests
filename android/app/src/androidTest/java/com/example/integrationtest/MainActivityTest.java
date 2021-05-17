6e2af4ee55c466e4750b7d2272a2220f245ea31e
Agnieszka Baumann <agnieszka.baumann@stepwise.pl>
2021-05-17 12:40:49 +0200

    Init

---
+++ android/app/src/androidTest/java/com/example/integrationtest/MainActivityTest.java
@@ -0,0 +1,14 @@
+package com.example.integrationtest;
+
+import androidx.test.rule.ActivityTestRule;
+import dev.flutter.plugins.integration_test.FlutterTestRunner;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+// import com.example.integrationtest.MainActivity;
+
+@RunWith(FlutterTestRunner.class)
+public class MainActivityTest {
+  @Rule
+  public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class, true, false);
+}
